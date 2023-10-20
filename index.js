d4de98ca8b2373892227db8c67fca37d3abc777d
Reginawambui <ngendoanita8@gmail.com>
2023-10-20 07:57:03 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet <=400){
+    return "This one is on me!";
+  }
+  else if(feet >2000){
+      return "I will gladly take your thirty bucks.";
+    }
+  else if (feet >2500){
+    return "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  if (city===NYC){
+    return "ok,sounds good."
+  }
+  else if() {
+     return "no go" 
+    }
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here
+  switch (tip){
+    case  "generous" :
+    return "Thank you so much."
+break
+
+case "not as generous":
+  return "Thank you."
+
+default :
+return "bye."
+  }
 }
